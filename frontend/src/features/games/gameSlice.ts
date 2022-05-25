98d94cc5904d0149d1fab3bc11171153d2172bc4
cliffgor <clierozigy@gmail.com>
2022-05-25 12:47:44 +0300

    Redux Set up created a slice.ts which processes actions from the backend and reducers to a specific state

--- frontend/src/features/games/gameSlice.ts
+++ frontend/src/features/games/gameSlice.ts
@@ -1,4 +1,7 @@
+import { PayloadAction, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
 import { Game } from "../../interfaces/Game";
+import axios from "axios";
 
 interface GameState {
     games: Game [] | null;
@@ -15,4 +18,25 @@ const initialState: GameState = {
 
 // actions are processes that get data from backend
 
+export const getGames = createAsyncThunk<Game[]> (
+    "games/getGames",
+    async (_, thunkAPI) => {
+        try {
+            const response = await axios.get("http://localhost:8080/api/games")
+            return response.data;
+        }catch (error){
+            return thunkAPI.rejectWithValue(error)
+        }
+    } 
+    )
+
 // reducers  -> reduce to a specific state/ changes state
+export const gameSlice = createSlice({
+    name:"games",
+    initialState,
+    reducers:  {
+        setGames: (state, action: PayloadAction<Game[]>) => {
+            state.games = action.payload
+        }
+    }
+})
